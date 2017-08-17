a64fbb28dfe866f53811972d3e9f869e0efc77e0
Santosh Nijalingappa <nnsantosh@gmail.com>
2017-08-16 22:49:29 -0700

    updated project

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,8 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.example.controller.GreetingController;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = GreetingController.class)
 public class DemoApplication {
 
 	public static void main(String[] args) {
