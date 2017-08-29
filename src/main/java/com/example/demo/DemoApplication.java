7e881cc72cde5c6266e4ffd762179138fa0465c0
Santosh Nijalingappa <nnsantosh@gmail.com>
2017-08-29 00:03:29 -0700

    used scanBasePackages in SpringBootApplication annotation

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-import com.example.controller.GreetingController;
-
-@SpringBootApplication
-@ComponentScan(basePackageClasses = GreetingController.class)
+@SpringBootApplication(scanBasePackages = "com.example")
 public class DemoApplication {
 
 	public static void main(String[] args) {
